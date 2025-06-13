f4c7dcc0117b7a51ddb08fac9ad4a70f605f9681
Your Name <you@example.com>
2025-06-13 10:55:28 -0600

    Conversion Finalizada

---
+++ Conversion de dolares a pesos mexicanos/Conversion de dolares a pesos mexicanos/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Conversion_de_dolares_a_pesos_mexicanos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.gbCentral = new System.Windows.Forms.GroupBox();
+            this.lblRespuesta = new System.Windows.Forms.Label();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.lblIngre = new System.Windows.Forms.Label();
+            this.pnlBotones = new System.Windows.Forms.Panel();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnConvertir = new System.Windows.Forms.Button();
+            this.gbCentral.SuspendLayout();
+            this.pnlBotones.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbCentral
+            // 
+            this.gbCentral.Controls.Add(this.lblRespuesta);
+            this.gbCentral.Controls.Add(this.txtCantidad);
+            this.gbCentral.Controls.Add(this.lblIngre);
+            this.gbCentral.Location = new System.Drawing.Point(21, 21);
+            this.gbCentral.Name = "gbCentral";
+            this.gbCentral.Size = new System.Drawing.Size(574, 167);
+            this.gbCentral.TabIndex = 0;
+            this.gbCentral.TabStop = false;
+            this.gbCentral.Text = "Conversion de Dolares a Pesos";
+            // 
+            // lblRespuesta
+            // 
+            this.lblRespuesta.AutoSize = true;
+            this.lblRespuesta.Location = new System.Drawing.Point(338, 135);
+            this.lblRespuesta.Name = "lblRespuesta";
+            this.lblRespuesta.Size = new System.Drawing.Size(84, 18);
+            this.lblRespuesta.TabIndex = 2;
+            this.lblRespuesta.Text = "Resultado";
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(59, 93);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(229, 24);
+            this.txtCantidad.TabIndex = 1;
+            // 
+            // lblIngre
+            // 
+            this.lblIngre.AutoSize = true;
+            this.lblIngre.Location = new System.Drawing.Point(24, 61);
+            this.lblIngre.Name = "lblIngre";
+            this.lblIngre.Size = new System.Drawing.Size(318, 18);
+            this.lblIngre.TabIndex = 0;
+            this.lblIngre.Text = "Ingrese la cantidad de dolares a convertir";
+            // 
+            // pnlBotones
+            // 
+            this.pnlBotones.Controls.Add(this.btnSalir);
+            this.pnlBotones.Controls.Add(this.btnLimpiar);
+            this.pnlBotones.Controls.Add(this.btnConvertir);
+            this.pnlBotones.Location = new System.Drawing.Point(620, 33);
+            this.pnlBotones.Name = "pnlBotones";
+            this.pnlBotones.Size = new System.Drawing.Size(240, 295);
+            this.pnlBotones.TabIndex = 2;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnSalir.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnSalir.Image = global::Conversion_de_dolares_a_pesos_mexicanos.Properties.Resources.icons8_eliminar_100;
+            this.btnSalir.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSalir.Location = new System.Drawing.Point(0, 199);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(240, 90);
+            this.btnSalir.TabIndex = 3;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnLimpiar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnLimpiar.Image = global::Conversion_de_dolares_a_pesos_mexicanos.Properties.Resources.icons8_limpiar_48__1_;
+            this.btnLimpiar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLimpiar.Location = new System.Drawing.Point(0, 105);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(240, 94);
+            this.btnLimpiar.TabIndex = 2;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnConvertir
+            // 
+            this.btnConvertir.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnConvertir.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnConvertir.Image = global::Conversion_de_dolares_a_pesos_mexicanos.Properties.Resources.icons8_filtro_relleno_91;
+            this.btnConvertir.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnConvertir.Location = new System.Drawing.Point(0, 0);
+            this.btnConvertir.Name = "btnConvertir";
+            this.btnConvertir.Size = new System.Drawing.Size(240, 105);
+            this.btnConvertir.TabIndex = 1;
+            this.btnConvertir.Text = "Convertir";
+            this.btnConvertir.UseVisualStyleBackColor = false;
+            this.btnConvertir.Click += new System.EventHandler(this.btnConvertir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1333, 623);
+            this.Controls.Add(this.pnlBotones);
+            this.Controls.Add(this.gbCentral);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.Text = "Conversion de Dolares  a Pesos Mexicanos";
+            this.gbCentral.ResumeLayout(false);
+            this.gbCentral.PerformLayout();
+            this.pnlBotones.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.GroupBox gbCentral;
+        private System.Windows.Forms.Button btnConvertir;
+        private System.Windows.Forms.Label lblRespuesta;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Label lblIngre;
+        private System.Windows.Forms.Panel pnlBotones;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnLimpiar;
+    }
+}
+
---
+++ Conversion de dolares a pesos mexicanos/Conversion de dolares a pesos mexicanos/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Conversion_de_dolares_a_pesos_mexicanos
+{
+    public partial class Form1 : Form
+    {
+        double cantDolares, pesos;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtCantidad.Clear();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnConvertir_Click(object sender, EventArgs e)
+        {
+            cantDolares = double.Parse(txtCantidad.Text);
+            if (cantDolares > 0)
+            {
+                pesos = cantDolares * 18;
+               lblRespuesta.Text = pesos.ToString("N2");
+            }
+            else
+            {
+                lblRespuesta.Text = "Ingrese numeros positivos";
+            }
+        }
+    }
+}
